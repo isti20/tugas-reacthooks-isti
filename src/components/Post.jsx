aa97d1d2e811294f5b5273faea30872c08953fd9
iis istiqomah <iisistiqomah20@gmail.com>
2023-04-07 01:22:29 +0700

    update styling header

---
+++ src/components/Post.jsx
@@ -0,0 +1,26 @@
+import { Container, Card, Button } from 'react-bootstrap';
+
+const Post = () => {
+
+    return (
+        <Container >
+            <Card style={{ width: '18rem', boxSizing: "border-box", paddingTop: "2rem"}} >
+                <Card.Img variant="top" src='/' style={{ width: '18rem' }} />
+                <Card.Body>
+                    <Card.Title style={{ fontSize: '18px' }}>Card Title</Card.Title>
+                    <small>Date</small>
+                    <Card.Text style={{ fontSize: '15px' }}>
+                        <small>Author</small>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="primary">
+                    <a href='/' >Read More</a>
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+}
+
+export default Post;
